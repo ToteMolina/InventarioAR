0fbeb3d2edebbadea4a5e32c3ae8303d21d5b4a0
Josué Iván Molina Romero <39289983+ToteMolina@users.noreply.github.com>
2026-05-16 12:28:29 -0600

    Integración de GPS, permisos de ubicáción y Google Play Services

--- app/src/main/java/com/example/inventarioar/HomeFragment.java
+++ app/src/main/java/com/example/inventarioar/HomeFragment.java
@@ -1,58 +1,55 @@
 package com.example.inventarioar;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.material.button.MaterialButton;
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView tvCoordenadas;
+    private MaterialButton btnObtenerUbicacion;
+    // El "motor" de Google para obtener coordenadas
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    // Lanzador moderno para pedir permiso en pantalla
+    private ActivityResultLauncher<String> requestPermissionLauncher;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        requestPermissionLauncher = registerForActivityResult(
+                new ActivityResultContracts.RequestPermission(),
+                isGranted -> {
+                    if (isGranted){
+                        obtenerUbicacion(); // el usuario le dió a permitir
+                    } else {
+                        Toast.makeText(getContext(), "Se requiere el permiso de de ubicación para esta función", Toast.LENGTH_SHORT).show();
+                        tvCoordenadas.setText("Permiso denegado");
+                    }
+                });
     }
 
     @Override
@@ -60,5 +57,42 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvCoordenadas = view.findViewById(R.id.tvCoordenadas);
+        btnObtenerUbicacion = view.findViewById(R.id.btnObtenerUbicacion);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+
+        btnObtenerUbicacion.setOnClickListener(v->verificarPermisoYObtenerUbicacion());
+    }
+
+    private void verificarPermisoYObtenerUbicacion(){
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            obtenerUbicacion();
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @SuppressLint("MissingPermission") // le decimos a android que ignore, ya que arriba lo validamos
+    private void obtenerUbicacion(){
+        tvCoordenadas.setText("Buscando ubicación...");
+
+        fusedLocationProviderClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+                .addOnSuccessListener(requireActivity(), location -> {
+                    // LA UBICACION PUEDE SER NULA SI EL GPS DEL CELULAR ESTÁ APAGADO O ES LA PRIMERA VEZ QUE SE USA
+                    if (location != null){
+                        double latitud = location.getLatitude();
+                        double longitud = location.getLongitude();
+                        tvCoordenadas.setText("Latitud: " + latitud + "\nLongitud: " + longitud);
+                    } else {
+                        tvCoordenadas.setText("Ubicación no disponible en este momento.");
+                    }
+                });
     }
 }
